c053af277d46aa69b9fb85cebb93fe9af4f72db4
ayushsharm07 <43581385+ayushsharm07@users.noreply.github.com>
2019-10-09 19:23:14 +0530

    Add files via upload

---
+++ k_neighbors_2.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+import warnings
+import random
+from collections import Counter
+style.use('fivethirtyeight')
+
+def k_nearest_neighbors(data, predict,k=5):
+    if len(data)>=k:
+        warnings.warn('k is set to a value less than total voting groups')
+    distances=[]
+    for group in data:
+        for features in data[group]:
+            euclidean_distance=np.linalg.norm(np.array(features)- np.array(predict))
+            distances.append([euclidean_distance, group])
+
+    votes=[i[1] for i in sorted(distances)[:k]]
+    vote_result=Counter(votes).most_common(1)[0][0]  #most common returns the most repeated element along with its count as [(element,times_repeated)]
+    return vote_result
+
+
+df=pd.read_csv('breast-cancer-wisconsin.data.txt')
+df.replace('?',-99999,inplace=True)
+df.drop(['id'],1,inplace=True)
+full_data=df.astype(float).values.tolist()    #tolist create list within list
+print(full_data[:5])
+random.shuffle(full_data)
+#print(20*'#')
+#print(full_data[:5])
+
+test_size=0.2
+train_set={2:[],4:[]}
+test_set={2:[],4:[]}
+train_data=full_data[:-int(test_size*len(full_data))]
+test_data= full_data[-int(test_size*len(full_data)):]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct=0
+total=0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote= k_nearest_neighbors(train_set,data,k=5)
+        if group==vote:
+            correct+=1
+        total+=1
+print('Accuracy:', correct/total)
+
+
+
